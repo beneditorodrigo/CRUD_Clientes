242d35994fef9933d1635811542ae818755b4f53
Benedito Rodrigo <rodrigodacunha7@gmail.com>
2022-09-07 15:47:59 -0300

    Criando ClienteController e implementado o método POST (cadastrar Cliente)

---
+++ src/main/java/com/beneditorodrigo/clientes/controllers/ClienteController.java
@@ -0,0 +1,20 @@
+package com.beneditorodrigo.clientes.controllers;
+
+import com.beneditorodrigo.clientes.entity.Cliente;
+import com.beneditorodrigo.clientes.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping("cliente")
+    public Cliente cadastrar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+}
